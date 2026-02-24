07cdb57944dfc110ba90d824c9d4652c168b8ab6
NEERAJ KUMAR BHARTI <135545145+Neeraj0788@users.noreply.github.com>
2026-02-24 23:20:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+ const sr = ScrollReveal ({
+    distance : '60px',
+    duration : 2500,
+    delay : 400,
+    reset : true
+ })
+ 
+ sr.reveal('.home h1',{delay: 200, origin: 'left'})
+ sr.reveal('.home p',{delay: 300, origin: 'left'})
+ sr.reveal('.home a',{delay: 600, origin: 'left'})
